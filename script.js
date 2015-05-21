614edb419b412a5d31332b58b33d657ebde09d03
shubham <shubhamdogra2011@gmail.com>
2015-05-21 21:22:02 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+//footer
+document.getElementById("foot01").innerHTML =
+"<p>&copy;  " + new Date().getFullYear() + " CompanyName. All rights reserved.</p>";
+
+//tabs
+document.getElementById("nav01").innerHTML =
+"<ul id='menu'>" +
+"<li><a href='aboutMe.jsp'>About Me</a></li>" +
+"<li><a href='projectsPosted.jsp'>Posted Projects</a></li>" +
+"<li><a href='projectsBidded.jsp'>Bids</a></li>" +
+"</ul>";
